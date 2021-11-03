5805c8a0273caebb6f325fa98258f7401c915981
aziz <mohamedaziz.bramli@esprit.tn>
2021-11-03 10:13:22 +0100

    ok

---
+++ sql/modele_relationel.sql
@@ -0,0 +1,52 @@
+-- Généré par Oracle SQL Developer Data Modeler 19.2.0.182.1216
+--   à :        2021-11-03 00:05:12 WAT
+--   site :      Oracle Database 11g
+--   type :      Oracle Database 11g
+
+
+
+
+
+-- Rapport récapitulatif Oracle SQL Developer Data Modeler : 
+-- 
+-- CREATE TABLE                             0
+-- CREATE INDEX                             0
+-- CREATE VIEW                              0
+-- ALTER TABLE                              0
+-- ALTER INDEX                              0
+-- ALTER VIEW                               0
+-- DROP TABLE                               0
+-- DROP INDEX                               0
+-- DROP VIEW                                0
+-- CREATE PACKAGE                           0
+-- CREATE PACKAGE BODY                      0
+-- CREATE PROCEDURE                         0
+-- CREATE FUNCTION                          0
+-- DROP PACKAGE                             0
+-- DROP PACKAGE BODY                        0
+-- DROP PROCEDURE                           0
+-- DROP FUNCTION                            0
+-- CREATE TRIGGER                           0
+-- ALTER TRIGGER                            0
+-- DROP TRIGGER                             0
+-- CREATE COLLECTION TYPE                   0
+-- CREATE STRUCTURED TYPE                   0
+-- CREATE STRUCTURED TYPE BODY              0
+-- DROP TYPE                                0
+-- CREATE SEQUENCE                          0
+-- ALTER SEQUENCE                           0
+-- DROP SEQUENCE                            0
+-- CREATE MATERIALIZED VIEW                 0
+-- DROP MATERIALIZED VIEW                   0
+-- CREATE SYNONYM                           0
+-- DROP SYNONYM                             0
+-- CREATE DIMENSION                         0
+-- DROP DIMENSION                           0
+-- CREATE CONTEXT                           0
+-- DROP CONTEXT                             0
+-- CREATE DIRECTORY                         0
+-- DROP DIRECTORY                           0
+
+-- 
+-- ERRORS                                   0
+-- WARNINGS                                 0
